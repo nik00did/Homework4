7c7d1ed640196e1f3d950223ceb370da739b470c
NIKITA <44376562+nik00did@users.noreply.github.com>
2019-08-04 19:09:53 +0300

    Add files via upload

---
+++ double_llist.js
@@ -0,0 +1,358 @@
+function DoubleNode (value) {
+    this.value = value;
+    this.next = null;
+    this.previous = null;
+}
+
+function DoubleLList () {
+    this._head = null;
+    this._tail = null;
+    this._length = 0;
+}
+
+DoubleLList.prototype = Object.create(List.prototype);
+DoubleLList.prototype.constructor = DoubleLList;
+
+DoubleLList.prototype.push = function (element) {
+
+    if (element !== undefined) {
+        const node = new DoubleNode(element);
+
+        if (this._head === null) {
+            this._head = this._tail =  node;
+        } else {
+            this._tail.next = node;
+            node.previous = this._tail;
+            this._tail = node;
+        }
+
+        this._length++;
+    }
+
+    return this._length;
+};
+
+DoubleLList.prototype.init = function (...elements) {
+    let array = [];
+
+    if (elements) {
+
+        for (let i = 0; i < elements.length; i++) {
+            this.push(elements[i]);
+            array[i] = elements[i];
+        }
+
+    }
+
+    return array;
+};
+
+DoubleLList.prototype.pop = function () {
+    let lastElement;
+
+    if (this._length !==0 ) {
+        lastElement = this._tail.value;
+        this._tail.next = null;
+        this._tail = this._tail.previous;
+        this._length--;
+    }
+
+    return lastElement;
+};
+
+DoubleLList.prototype.shift = function () {
+    let firstElement;
+
+    if (this._length !== 0) {
+        firstElement = this._head.value;
+        this._head = this._head.next;
+        this._head.previous = null;
+        this._length--;
+    }
+
+    return firstElement;
+};
+
+DoubleLList.prototype.unshift = function (element) {
+    let node;
+
+    if (element ) {
+        node = new DoubleNode(element);
+        node.next = this._head;
+        this._head = node;
+        this._head.previous = node;
+        this._length++;
+    }
+
+    return this._length;
+};
+
+DoubleLList.prototype.getSize = function () {
+    return this._length;
+};
+
+DoubleLList.prototype.getArray = function () {
+    let array = [];
+    let temp = this._head;
+
+    for (let i = 0; i < this._length; i++) {
+
+        if(temp !== null) {
+            array[i] = temp.value;
+            temp = temp.next;
+        }
+
+    }
+
+    return array;
+};
+
+DoubleLList.prototype.toString = function () {
+    let string = '"[';
+    let temp;
+
+    if (this._root !== null) {
+        temp = this._head;
+    } else {
+        return '"[]"';
+    }
+
+    for (let i = 0; i < this._length; i++) {
+
+        if (temp.next !== null && i !== this._length - 1) {
+            string += temp.value;
+            temp = temp.next;
+            string += ', ';
+        }
+
+    }
+
+    string += temp.value + ']"';
+
+    return string;
+};
+
+DoubleLList.prototype.slice = function (startIndex, finishIndex) {
+    if (this._length === 0 || startIndex < 0 ) {
+        return this.getArray();
+    }
+
+    if (startIndex > this._length) {
+        return [];
+    }
+
+    if (finishIndex < startIndex) {
+        return [];
+    }
+
+    let start = startIndex;
+    let finish = finishIndex;
+    let array = [];
+    let count = 0;
+    let temp = this._head;
+
+    if(!startIndex && startIndex !== 0) {
+        start = 0;
+    }
+
+    if ((!finishIndex && finishIndex !== 0) || finishIndex > this._length) {
+        finish = this._length;
+    }
+
+    for (let i = 0; i < finish; i++) {
+
+        if (i === start) {
+            array[count] = temp.value;
+            count++;
+            start++;
+        }
+        temp = temp.next;
+    }
+
+    return array;
+};
+
+DoubleLList.prototype.splice = function (startIndex, amountDelete, ...insertElement) {
+    let definedStartIndex = 0;
+    let definedAmountDelete = 0;
+    let definedInsertElem = [];
+    let spliceArray = [];
+    let tempSize = 0;
+
+    if (!startIndex) {
+        definedStartIndex = 0;
+    } else if (!!Number(startIndex)) {
+
+        if (startIndex > this._length) {
+            definedStartIndex = this._length;
+        } else {
+            definedStartIndex = parseInt(startIndex);
+        }
+
+    } else {
+        return spliceArray;
+    }
+
+    if (!amountDelete || amountDelete <= 0) {
+        definedAmountDelete = 0;
+    } else if (!!Number(amountDelete)) {
+        definedAmountDelete = parseInt(amountDelete);
+    } else {
+        definedAmountDelete = 0;
+    }
+
+    if (!insertElement) {
+        definedInsertElem = null;
+    } else {
+
+        for (let i = 0; i < insertElement.length; i++) {
+            definedInsertElem[i] = insertElement[i];
+        }
+
+    }
+
+    spliceArray = this.slice(definedStartIndex,definedStartIndex + definedAmountDelete);
+
+    let curIndex = 0;
+    let temp = this._head;
+
+    while (curIndex < definedStartIndex) {
+        curIndex++;
+        temp = temp.next;
+    }
+
+    let afterSplicedTemp = temp;
+
+    while (curIndex < definedStartIndex + definedAmountDelete && afterSplicedTemp !== null) {
+        curIndex++;
+        afterSplicedTemp = afterSplicedTemp.next;
+    }
+
+    let tempAfterSplicedTemp = afterSplicedTemp;
+
+    while (tempAfterSplicedTemp !== null) {
+        tempAfterSplicedTemp = tempAfterSplicedTemp.next;
+        tempSize++;
+    }
+
+    let tempRoot = null;
+
+    for (let i = 0; i < definedInsertElem.length; i++ ) {
+        const node = new DoubleNode(definedInsertElem[i]);
+
+        if (tempRoot === null) {
+            tempRoot = node;
+        } else {
+            let tempNode = tempRoot;
+
+            while (tempNode.next) {
+                tempNode = tempNode.next;
+            }
+
+            tempNode.next = node;
+        }
+
+        tempSize++;
+    }
+
+    for (let i = definedInsertElem.length - 1; i >= 0; i--) {
+        let node = new DoubleNode(definedInsertElem[i]);
+        tempRoot = afterSplicedTemp;
+        node.next = tempRoot;
+        afterSplicedTemp = node;
+        tempSize++;
+    }
+
+    let leftPart = this.slice(0, definedStartIndex);
+
+    for (let i = leftPart.length - 1; i >= 0; i--) {
+        let node = new DoubleNode(leftPart[i]);
+        this._root = afterSplicedTemp;
+        node.next = this._root;
+        afterSplicedTemp = node;
+        tempSize++;
+    }
+
+    this._length = tempSize;
+    this._head = afterSplicedTemp;
+
+    return spliceArray;
+};
+
+DoubleLList.prototype.get = function (index) {
+    let temp;
+
+    if (this._length !== 0) {
+        temp = this._head;
+    } else {
+        return undefined;
+    }
+
+    let curIndex = 0;
+
+    if (index > this._length || index < 0 ) {
+        return undefined;
+    } else {
+
+        while (curIndex < index && temp !== null) {
+            curIndex++;
+            temp = temp.next;
+        }
+
+    }
+
+    return temp.value;
+};
+
+DoubleLList.prototype.set = function (index, value) {
+    let temp;
+
+    if (this._length !== 0 && index < this._length && index >= 0) {
+        temp = this._head;
+    } else {
+        return ;
+    }
+
+    let curIndex = 0;
+
+    while (curIndex < index && temp.next !== null ) {
+        curIndex++;
+        temp = temp.next;
+    }
+
+    temp.value = value;
+};
+
+DoubleLList.prototype.sort = function (comparator = sortFunc0) {
+    let current = this._head;
+
+    for (let i = 0; i < this._length; i++) {
+
+        for (let j = 0; j < this._length - 1; j++) {
+            let result = comparator(current.value, current.next.value);
+            let temp = 0;
+
+            if (result > 0) {
+                temp = current.next.value;
+                current.next.value = current.value;
+                current.value = temp;
+            }
+
+            if (current.next) {
+                current = current.next;
+            } else {
+                break;
+            }
+
+        }
+
+        current = this._head;
+    }
+
+    return this.getArray();
+};
+
+let dllist = new DoubleLList();
+console.log(dllist.init(0, 1, 2, 3, 4));
+console.log(dllist.splice(2, 4, '*', '#'));
+console.log(dllist.getArray());
---
+++ llist.js
@@ -0,0 +1,379 @@
+function Node (value) {
+    this.value = value;
+    this.next = null;
+}
+
+function LList () {
+    this._root = null;
+    this._length = 0;//List.apply(this);
+}
+
+LList.prototype = Object.create(List.prototype);
+LList.prototype.constructor = LList;
+
+LList.prototype.push = function (element) {
+
+    if (element !== undefined) {
+        const node = new Node(element);
+
+        if (this._root === null) {
+            this._root = node;
+        } else {
+            let tempNode = this._root;
+
+            while (tempNode.next) {
+                tempNode = tempNode.next;
+            }
+
+            tempNode.next = node;
+        }
+
+        this._length++;
+    }
+
+    return this._length;
+};
+
+LList.prototype.pop = function () {
+    let temp;
+
+    if (this._root !== null){
+        temp = this._root;
+    } else {
+        return undefined;
+    }
+
+    let beforeElem = temp;
+    let returnTemp;
+
+    for (let i = 0; i < this._length; i++) {
+
+        if (temp.next !== null) {
+            beforeElem = temp;
+            temp = temp.next;
+        } else {
+            beforeElem.next = null;
+            returnTemp = temp.value;
+            this._length--;
+        }
+
+    }
+
+    return returnTemp;
+};
+
+LList.prototype.shift = function () {
+
+    if (this._length === 0 ) {
+        return undefined;
+    } else {
+        let temp = this._root;
+
+        this._root = temp.next;
+        this._length--;
+
+        return temp.value;
+    }
+
+};
+
+LList.prototype.unshift = function (element) {
+    let node;
+
+    if (element) {
+        node = new Node(element);
+        node.next = this._root;
+        this._root = node;
+        this._length++;
+    }
+
+    return this._length;
+};
+
+LList.prototype.init = function (...elements) {
+    let array = [];
+
+    if (elements) {
+
+        for (let i = 0; i < elements.length; i++) {
+            this.push(elements[i]);
+            array[i] = elements[i];
+        }
+
+    }
+
+    return array;
+};
+
+
+
+LList.prototype.getArray = function () {
+    let array = [];
+    let temp = this._root;
+
+    for (let i = 0; i < this._length; i++) {
+
+        if(temp !== null) {
+            array[i] = temp.value;
+            temp = temp.next;
+        }
+
+    }
+
+    return array;
+};
+
+LList.prototype.getSize = function () {
+    return this._length;
+};
+
+LList.prototype.toString = function () {
+    let string = '"[';
+    let temp;
+
+    if (this._root !== null) {
+        temp = this._root;
+    } else {
+        return '"[]"';
+    }
+
+    for (let i = 0; i < this._length; i++) {
+
+        if (temp.next !== null && i !== this._length - 1) {
+            string += temp.value;
+            temp = temp.next;
+            string += ', ';
+        }
+
+    }
+
+    string += temp.value + ']"';
+
+    return string;
+};
+
+LList.prototype.slice = function (startIndex, finishIndex) {
+    if (this._length === 0 || startIndex < 0 ) {
+        return this.getArray();
+    }
+
+    if (startIndex > this._length) {
+        return [];
+    }
+
+    if (finishIndex < startIndex) {
+        return [];
+    }
+
+    let start = startIndex;
+    let finish = finishIndex;
+    let array = [];
+    let count = 0;
+    let temp = this._root;
+
+    if(!startIndex && startIndex !== 0) {
+        start = 0;
+    }
+
+    if ((!finishIndex && finishIndex !== 0) || finishIndex > this._length) {
+        finish = this._length;
+    }
+
+    for (let i = 0; i < finish; i++) {
+
+        if (i === start) {
+            array[count] = temp.value;
+            count++;
+            start++;
+        }
+
+        temp = temp.next;
+    }
+
+    return array;
+};
+
+LList.prototype.splice = function (startIndex, amountDelete, ...insertElement) {
+    let definedStartIndex = 0;
+    let definedAmountDelete = 0;
+    let definedInsertElem = [];
+    let spliceArray = [];
+    let tempSize = 0;
+
+    if (!startIndex) {
+        definedStartIndex = 0;
+    } else if (!!Number(startIndex)) {
+
+        if (startIndex > this._length) {
+            definedStartIndex = this._length;
+        } else {
+            definedStartIndex = parseInt(startIndex);
+        }
+
+    } else {
+        return spliceArray;
+    }
+
+    if (!amountDelete || amountDelete <= 0) {
+        definedAmountDelete = 0;
+    } else if (!!Number(amountDelete)) {
+        definedAmountDelete = parseInt(amountDelete);
+    } else {
+        definedAmountDelete = 0;
+    }
+
+    if (!insertElement) {
+        definedInsertElem = null;
+    } else {
+
+        for (let i = 0; i < insertElement.length; i++) {
+            definedInsertElem[i] = insertElement[i];
+        }
+
+    }
+
+    spliceArray = this.slice(definedStartIndex,definedStartIndex + definedAmountDelete);
+
+    let curIndex = 0;
+    let temp = this._root;
+
+    while (curIndex < definedStartIndex) {
+        curIndex++;
+        temp = temp.next;
+    }
+
+    let afterSplicedTemp = temp;
+
+    while (curIndex < definedStartIndex + definedAmountDelete && afterSplicedTemp !== null) {
+        curIndex++;
+        afterSplicedTemp = afterSplicedTemp.next;
+    }
+
+    let tempAfterSplicedTemp = afterSplicedTemp;
+
+    while (tempAfterSplicedTemp !== null) {
+        tempAfterSplicedTemp = tempAfterSplicedTemp.next;
+        tempSize++;
+    }
+
+    let tempRoot = null;
+
+    for (let i = 0; i < definedInsertElem.length; i++ ) {
+        const node = new Node(definedInsertElem[i]);
+
+        if (tempRoot === null) {
+            tempRoot = node;
+        } else {
+            let tempNode = tempRoot;
+
+            while (tempNode.next) {
+                tempNode = tempNode.next;
+            }
+
+            tempNode.next = node;
+        }
+
+        tempSize++;
+    }
+
+    for (let i = definedInsertElem.length - 1; i >= 0; i--) {
+        let node = new Node(definedInsertElem[i]);
+        tempRoot = afterSplicedTemp;
+        node.next = tempRoot;
+        afterSplicedTemp = node;
+        tempSize++;
+    }
+
+    let leftPart = this.slice(0, definedStartIndex);
+
+    for (let i = leftPart.length - 1; i >= 0; i--) {
+        let node = new Node(leftPart[i]);
+        this._root = afterSplicedTemp;
+        node.next = this._root;
+        afterSplicedTemp = node;
+        tempSize++;
+    }
+
+    this._length = tempSize;
+    this._root = afterSplicedTemp;
+
+    return spliceArray;
+};
+
+LList.prototype.get = function (index) {
+    let temp;
+
+    if (this._length !== 0) {
+        temp = this._root;
+    } else {
+        return undefined;
+    }
+
+    let curIndex = 0;
+
+    if (index > this._length || index < 0 ) {
+        return undefined;
+    } else {
+
+        while (curIndex < index && temp !== null) {
+            curIndex++;
+            temp = temp.next;
+        }
+
+    }
+
+    return temp.value;
+};
+
+LList.prototype.set = function (index, value) {
+    let temp;
+
+    if (this._length !== 0 && index < this._length && index >= 0) {
+        temp = this._root;
+    } else {
+        return ;
+    }
+
+    let curIndex = 0;
+
+    while (curIndex < index && temp.next !== null ) {
+        curIndex++;
+        temp = temp.next;
+    }
+
+    temp.value = value;
+};
+
+LList.prototype.sort = function (comparator = sortFunc0) {
+    let current = this._root;
+
+    for (let i = 0; i < this._length; i++) {
+
+        for (let j = 0; j < this._length - 1; j++) {
+            let result = comparator(current.value, current.next.value);
+            let temp = 0;
+
+            if (result > 0) {
+                temp = current.next.value;
+                current.next.value = current.value;
+                current.value = temp;
+            }
+
+            if (current.next) {
+                current = current.next;
+            } else {
+                break;
+            }
+
+        }
+
+        current = this._root;
+    }
+
+    return this.getArray();
+};
+//
+// let llist = new LList();
+// console.log(llist.init(0, 1, 2));
+// console.log(llist.push(2));
+// console.log(llist.getArray());
